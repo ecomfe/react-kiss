522eb35f6b96182f50f85789287c08f48d08b5a5
otakustay <otakustay@gmail.com>
2018-08-06 13:29:30 +0800

    管理一个同步的状态用于工作流中的getState函数

--- src/utils/createRegionComponent.js
+++ src/utils/createRegionComponent.js
@@ -2,19 +2,31 @@ import {Component} from 'react';
 import {mapValues} from 'lodash';
 import consumeWorkflow from './consumeWorkflow';
 
+const applyStateChange = (state, stateChange) => {
+    const patch = typeof stateChange === 'function' ? stateChange(state) : stateChange;
+    return {...state, ...patch};
+};
+
 export default (initialState, workflows, displayName) => class extends Component {
 
     static displayName = displayName;
 
     state = initialState;
 
+    // Since `setState` doesn't guarantee synchronously update of state,
+    // we need to manage a state to ensure `getState` function returns correct state object
+    stateInSync = initialState;
+
     workflows = null;
 
     constructor(props) {
         super(props);
 
-        const getState = () => this.state;
-        const applyNext = this.setState.bind(this);
+        const getState = () => this.stateInSync;
+        const applyNext = stateChange => {
+            this.stateInSync = applyStateChange(this.stateInSync, stateChange);
+            this.setState(this.stateInSync);
+        };
         const linkWorkflowToState = workflow => {
             return payload => consumeWorkflow(workflow, payload, getState, applyNext);
         };
